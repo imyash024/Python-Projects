b35b07080288e8b91871eb5b94b91d672c457609
imyash024 <yjaiswal593@gmail.com>
2025-05-10 22:19:21 +0530

    Added comments

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,8 @@
+# print the statement in double quotes on the screen
 print("Welcome to the Band Name Generator.")
+
+#Prompt the user to input the name of city in which they grow in and the name of pet. Inputs in python are by default a string
 city = input("Which city did you grow up in?\n")
 pet = input("What is the name of a pet?\n")
+# Geneates finally the band name generator by concatening (adding) two string
 print("Your band name could be " + city + " " + pet)
