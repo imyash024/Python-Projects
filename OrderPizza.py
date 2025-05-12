e74ab92b9ca1ed7ec42573f0aba478ff163c6bd9
imyash024 <yjaiswal593@gmail.com>
2025-05-12 16:15:14 +0530

    Added comments into orderPizza Project

--- OrderPizza.py
+++ OrderPizza.py
@@ -1,9 +1,12 @@
 print("Welcome to Python Pizza Deliveries!")
+
+# takes size, pepperoni and extra cheese choices as an input.
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 bill = 0
 
+# based on choice of user, bill is calculated using if else conditional statements.
 if size == "S":
     bill += 15
     if pepperoni == "Y":
@@ -20,6 +23,7 @@ elif size == "L":
 if extra_cheese == "Y":
     bill += 1
 
+# bill is displayed to the user.
 print(f"Your final bill is: ${bill}.")
 
 
